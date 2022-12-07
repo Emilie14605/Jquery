a1080d79c3862b685eb1514236da13d559a46971
emilie <emilieroger360@gmail.com>
2022-12-07 16:30:39 +0100

    fin

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,31 @@ $(document).ready(function(){
         $('#content_ajax').load('assets/ajax/content.html');
     })
     $('.drag').draggable();
+    //Datepicker (choisir la date)
+    let dateFormat = 'dd/mm/yy',
+    from = $('#datedebut').datepicker({
+        defaultDate: '+1w',
+        changeMonth: true,
+        numberOfMonths: 2
+    }).on("change",function(){
+        to.datepicker("option","minDate",getDate(this));
+    }),
+    to = $('#datefin').datepicker({
+        defaultDate: '+1w',
+        changeMonth: true,
+        numberOfMonths: 2
+    }).on("change",function(){
+        from.datepicker("option","maxDate",getDate(this));
+    });
+    function getDate(element){
+        let date;
+        try{
+            date = $.datepicker.parseDate(dateFormat,element.value);
+        }catch(error){
+            date = null;
+        }
+        return date;
+    }
 });
 //L'image va suivre la souris
 // $(document).bind('mousemove',function(e){
